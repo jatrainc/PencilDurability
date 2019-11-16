aff9ac839a6969b7942a765cc762953aef2f7ce7
jatrainc <jatrainc@gmail.com>
2019-11-15 19:43:10 -0600

    Added Unit Test Project

---
+++ PencilDurabilityTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace PencilDurabilityTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestPencilWritesToASheetOfPaper()
+        {
+            Assert.Pass();
+        }
+    }
+}
